8a0a2c40d422d10372872020573e38237327931a
Derisbourg Arnaud <derisbourgarnaud@gmail.com>
2020-03-10 14:48:19 +0100

    commit du 10/03/20

--- src/java/com/arnaud/poo/modes/Defenseur.java
+++ src/java/com/arnaud/poo/modes/Defenseur.java
@@ -7,22 +7,25 @@ import org.apache.logging.log4j.Logger;
 
 
 public class Defenseur extends GestionJeu {
-  
+
   private static final Logger logger = LogManager.getLogger(Defenseur.class);
 
-  public void humainSwitchDefenseur(Player humain) {
+    public Defenseur() {
+        super();
+    }
+
+    public void humainSwitchDefenseur(Player humain) {
     Player ia = new Player("CPU",5);
-    GestionJeu gestionJeu = new GestionJeu();
-    gestionJeu.init();
+    this.init();
     String reponse = "";
     String modele = "";
-    
+
     System.out.println("vous avez choisi le mode défenseur");
     System.out.println("vous avez cette fois le rôle de défenseur vous devez saisir une combinaison de " + GestionConfiguration.tailleCode +
       " chiffre(s) \r\nL'intelligence artificielle de l’ordinateur doit faire une proposition d’une combinaison de X chiffres (c’est le rôle attaquant).");
 
     // Le joueur définie une combinaison de x chiffre
-    String combinaisonJoueur = gestionJeu.definirCombinaison();
+    String combinaisonJoueur = this.definirCombinaison();
     humain.setVie(GestionConfiguration.nbrEssaisMAX); // TODO add Player class instead of Humain(finish)
     // affichage de la solution dans le mode developpeur
 
@@ -36,13 +39,13 @@ public class Defenseur extends GestionJeu {
         System.out.println("Elle propose une combinaison de " + GestionConfiguration.tailleCode + " chiffre(s)");
 
         // génération d'une réponse par l'IA et affichage dans la console
-        reponse = gestionJeu.genererReponse(reponse, modele);
+        reponse = this.genererReponse(reponse, modele);
 
         System.out.println("Définissez un modele pour chaques chiffres avec les symbôles +, - et = :");
         System.out.println(reponse);
 
         // comparaison de la réponse avec la combinaison à trouver pour générer un modèle
-        modele = gestionJeu.definirModele();
+        modele = this.definirModele();
         ia.setVie(ia.getVie() - 1);
         if (ia.getVie() == 0) {
           System.out.println("Vous avez gagner l'ia n'a pas trouver la combinaison retour au menu");
